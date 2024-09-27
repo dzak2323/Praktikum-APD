5e008533751d5e0988e7c853744025593a259646
dzak2323 <23dzakie@gmail.com>
2024-09-27 14:51:41 +0800

    Add files via upload

---
+++ PRAKTIKUM_APD/KELAS/pertemuan 4/2409106056_Muhammad Dzaki Rifa'I_B1'24_pertemuan ke 3.py
@@ -0,0 +1,71 @@
+#==========================================================================================
+##if cuaca == "cerah"
+#print("aku jalan")
+# uang = (input("berapa duitmu?: "))
+#==========================================================================================
+## if uang:
+#     print("aku kaya")
+# else:
+#     print("aku butuh uang
+#==========================================================================================
+## bilangan = -3
+
+# if bilangan < 0:
+#     print("angka ini bilangan negatif")
+# else:
+#     print("angka ini bilangan positif")
+#===========================================================================================
+# umur = int(input("Masukan umur kamu: "))
+
+# if umur <= 10:
+#     print( "Umur Anda termasuk kategori anak-anak")
+# elif umur <= 18:
+#     print( "Umur Anda termasuk kategori remaja")
+# elif umur <= 35:
+#     print( "Umur Anda termasuk kategori dewasa")
+# elif umur <= 60:
+#     print( "Umur Anda termasuk kategori lansia")
+# else:
+#     print("Tua")
+#==========================================================================================
+# nilai = int(input("Berapa nilai mu: "))
+
+# if nilai >= 80:
+#     if nilai >= 85:
+#         print("A+")
+#     else:
+#         print("A-")
+# else:
+#     print("Anda tidak lulus")
+#==========================================================================================
+# print ("""
+# Masukan pilihan anda
+# 1.Nonton film
+# 2.Ngoding
+# 3.Keluar""")
+
+# Pilihan=input("masukan pilihan anda: ")
+ 
+# if Pilihan == "1":
+#     print("lagi nonton wal")
+# elif Pilihan == "2":
+#     print("lagi ngoding wal")
+# elif Pilihan == "3":
+#     print("Keluar dari aplikasi")
+# else:
+#     print("tidak terdapat di dalam pilihan")
+#==========================================================================================
+# angka = 10
+# if angka %2==0:
+#     print("genap")
+# if angka > 0:
+#     print("Positif")
+
+#============================== PERBEDAAN IF IF DAN IF ELIF ===============================
+
+# if angka %2==0:
+#     print("genap")
+# elif angka > 0:
+#     print("Positif")
+#==========================================================================================
+
